7006d9a9736fb4ab8d5b76435cce117045ea977b
Hariprasath_M <143427323+Hariprasath76@users.noreply.github.com>
2024-04-20 15:39:23 +0530

    JP_Prototype_02

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topbound = 30;
+    private float lowerbound = -10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If an Object goes past the Players view in the game, remove that objects.
+        if(transform.position.z > topbound)
+        {
+            Destroy(gameObject);
+        }
+        else if(transform.position.z < lowerbound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+    }
+}
